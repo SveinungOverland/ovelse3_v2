830246eb52a721248093fab64214e269bc3153ba
Sveinung Øverland <sveinungg.overlandd@gmail.com>
2018-09-09 03:32:06 +0200

    fix 3

--- server/app.js
+++ server/app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
-const path = require("path");
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('/', function (req, res) {
