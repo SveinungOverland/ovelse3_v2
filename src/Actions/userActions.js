5e8abe2f2a3bdd030a87c7ab313f1c240315f1be
Sveinung Øverland <sveinungskole@gmail.com>
2018-09-12 02:47:28 +0200

    even more debugging

--- src/Actions/userActions.js
+++ src/Actions/userActions.js
@@ -4,12 +4,16 @@ import { login } from '../services';
 export function loginUser(username, password) {
     console.log("Asking for login promise");
     return dispatch => {
+        console.log("Dispatching request");
         dispatch(request());
 
+        console.log("Running login from services");
         login(username, password)
             .then(user => {
+                console.log("Login was successful, dispatching success");
                 dispatch(success(user));
             }, error => {
+                console.log("Login was unsuccessful, dispatching failure");
                 dispatch(failure(error.toString()));
             })
     };
