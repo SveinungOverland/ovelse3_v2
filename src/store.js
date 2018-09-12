8bf82797338eaddb1f9427c5c6340c9de27a78de
Sveinung Øverland <sveinungskole@gmail.com>
2018-09-12 03:18:04 +0200

    oi

--- src/store.js
+++ src/store.js
@@ -2,10 +2,9 @@ import { applyMiddleware, createStore } from 'redux';
 
 import reducer from "./Reducers";
 
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 
-const middleware = applyMiddleware(promise(), thunk, logger());
+const middleware = applyMiddleware(promise(), thunk);
 
 export default createStore(reducer, middleware);
